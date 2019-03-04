02cc4d625b79ebd7aa7e1e3b208086e4cd1a3c5d
p1us2er0 <p1us3inus2er0@gmail.com>
2019-03-05 00:09:59 +0900

    [freegen][kvs]jedis cluster.

---
+++ src/main/resources/kvs/schema/kvs_pool_rule.js
@@ -0,0 +1,8 @@
+// _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// KVSFluteGen your rule settings as ECMAScript5 (related to KvsRule.js in freegen)
+// _/_/_/_/_/_/_/_/_/_/
+// =======================================================================================
+//                                                                                  Option
+//                                                                                  ======
+// comment out when automatically generating classes for cluster.
+//kvsRule.cluster = true;
