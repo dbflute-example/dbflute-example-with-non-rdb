41bd732169d76e7c81a1b8602a3614e7c8a9c272
p1us2er0 <p1us3inus2er0@gmail.com>
2019-10-27 20:03:54 +0900

    [freegen][remoteapi]Generate parent class for the same definition bean.

--- src/main/resources/remoteapi/schema/remoteapi_schema_petstore_rule.js
+++ src/main/resources/remoteapi/schema/remoteapi_schema_petstore_rule.js
@@ -1,3 +1,7 @@
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // RemoteApiGen your rule settings as ECMAScript5 (related to RemoteApiRule.js in freegen)
 // _/_/_/_/_/_/_/_/_/_/
+// =======================================================================================
+//                                                                            Param/Return
+//                                                                            ============
+remoteApiRule.beanExtendsDefinitionGeneration = true;
